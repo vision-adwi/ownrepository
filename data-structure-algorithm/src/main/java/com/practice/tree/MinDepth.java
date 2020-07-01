aab65bb5d7c86f50d4edc4f836c2e450a9e3d483
KLOUD9DC\anurag.d <anuragx.dwivedi@kohls.com>
2020-07-01 12:37:33 +0530

    ..

--- data-structure-algorithm/src/main/java/com/practice/tree/MinDepth.java
+++ data-structure-algorithm/src/main/java/com/practice/tree/MinDepth.java
@@ -1,13 +1,24 @@
 package com.practice.tree;
-
+/*
+Leetcode#111. Minimum Depth of Binary Tree
+Given a binary tree, find its minimum depth.
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+*/
 public class MinDepth {
     public int minDepth(TreeNode root) {
-        return getMinDepth(root, 0, Integer.MAX_VALUE);
+    	if(root != null)
+    		return getMinDepth(root, 0, Integer.MAX_VALUE);
+    	else
+    		return 0;
     }
     
     private int getMinDepth(TreeNode theNode, int depth, int minDepth) {
     	if(theNode == null) 
-    		return depth < minDepth ? depth : minDepth;
+    		return minDepth;
+    	
+    	if(theNode.left == null && theNode.right == null) {
+    		return (depth + 1) < minDepth ? (depth + 1) : minDepth;
+    	}
     	
     	minDepth = getMinDepth(theNode.left, depth + 1, minDepth);
     	minDepth = getMinDepth(theNode.right, depth + 1, minDepth);
